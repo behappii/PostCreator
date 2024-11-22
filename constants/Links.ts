51b618971cd88f3342ec2768f30c7ac99f36de90
stephan <ctepan.boyarkin@yandex.ru>
2024-11-22 12:23:18 +1000

    vercel deploy

--- constants/Links.ts
+++ constants/Links.ts
@@ -1,2 +1,3 @@
-export const DataBase = 'https://4k9p6n5r-3000.euw.devtunnels.ms'
+// export const DataBase = 'https://4k9p6n5r-3000.euw.devtunnels.ms'
 // export const DataBase = 'http://192.168.1.106:3000'
+export const DataBase = 'https://fakeservertest.vercel.app'
